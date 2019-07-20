e936e43df4231d06a99b46d804faa1dfca60f6a3
Mike Hahn <hahnbytes@gmail.com>
2019-07-20 19:41:03 -0400

    Add files via upload

--- src/synchk/SynChk.java
+++ src/synchk/SynChk.java
@@ -5,20 +5,34 @@ import iconst.NodeCellTyp;
 import page.Node;
 import page.Page;
 import page.Store;
-import scancoop.ScanCoop;
+import scanpsyva.ScanPsyva;
 
 public class SynChk {
 	
-	private ScanCoop scan;
+	private ScanPsyva scan;
 	private Store store;
 	private boolean isZparen;
+	private static final int ABPHASE = 100;
 
-	public SynChk(ScanCoop scan, Store store) {
+	public SynChk(ScanPsyva scan, Store store) {
 		this.scan = scan;
 		this.store = store;
 	}
 	
 	private void out(String msg) {
+		scan.out(msg);
+	}
+	
+	private void oerr(int nodep, String msg) {
+		int lineno;
+		String preLineNoStr;
+		
+		lineno = store.lookupLineNo(nodep);
+		if (lineno > 0) {
+			preLineNoStr = "Line " + lineno + " - ";
+			msg = preLineNoStr + msg;
+		}
+		scan.out("Syntax Error:");
 		scan.out(msg);
 	}
 	
@@ -93,7 +107,7 @@ public class SynChk {
 		return tokenCount + (listCount << 16);
 	}
 	
-	public boolean isValidCoop() {
+	public boolean isValidPsyva() {
 		int stmtCount = 0;
 		int count;
 		int rightp, downp;
@@ -107,7 +121,7 @@ public class SynChk {
 		int stmtNo = 0;
 		int doBlkCount = 0;
 
-		out("Top of isValidCoop");
+		out("Top of isValidPsyva");
 		rightp = scan.getRootNodep();
 		if (rightp == 0) {
 			return false;
@@ -204,6 +218,8 @@ public class SynChk {
 		boolean isGlbDef = false;
 		int currPhaseNo = phaseNo;
 		int rightq;
+		int initp = rightp;
+		String phaseDesc;
 
 		while (rightp != 0) {
 			page = store.getPage(rightp);
@@ -216,7 +232,10 @@ public class SynChk {
 			if (first) {
 				out("Statement kwd = " + kwtyp);
 				currPhaseNo = getPhaseNo(kwtyp);
+				phaseDesc = getPhaseDesc(currPhaseNo);
 				if (currPhaseNo < phaseNo) {
+					oerr(rightp, "Top-level " + phaseDesc + 
+						" encountered unexpectedly");
 					return -1;
 				}
 				rightq = rightp;
@@ -224,6 +243,10 @@ public class SynChk {
 				switch (currPhaseNo) {
 				case 1:
 					rightq = chkImportStmt(rightp, kwtyp);
+					if (rightq == -1) {
+						oerr(initp, "Keyword 'import' followed by " +
+							"no module names");
+					}
 					break;
 				case 2:
 					if (isGlbDef) {
@@ -241,7 +264,9 @@ public class SynChk {
 					rightq = chkClassStmt(rightp, kwtyp);
 					break;
 				default:
-					if (currPhaseNo <= 0) {
+					if (currPhaseNo <= ABPHASE) {
+						oerr(rightq, "Keyword 'abdefun' encountered at " +
+							"top level unexpectedly");
 						rightq = -1;
 					}
 				}
@@ -280,10 +305,22 @@ public class SynChk {
 		case ENUM:
 		case IENUM:
 			return 4;
+		case ABDEFUN:
+			return ABPHASE;
 		case ZPAREN:
 			return 9999;
 		default:
 			return -1;
+		}
+	}
+	
+	private String getPhaseDesc(int phaseNo) {
+		switch (phaseNo) {
+		case 1: return "module import(s)";
+		case 2: return "global(s)";
+		case 3: return "global function definition(s)";
+		case 4: return "class definition(s)";
+		default: return "post-class definitions phase";
 		}
 	}
 	
@@ -300,7 +337,7 @@ public class SynChk {
 		Node node, subNode;
 		KeywordTyp kwtyp;
 		NodeCellTyp celltyp;
-		int currp;
+		int currp, ascurrp;
 		int modCount = 0;
 		int savep = rightp;
 
@@ -316,6 +353,8 @@ public class SynChk {
 				out("celltyp = ID");
 			}
 			else if (celltyp != NodeCellTyp.PTR) {
+				oerr(rightp, "Keyword 'import' followed by invalid text: " +
+					kwtyp.toString());
 				return 0;
 			}
 			else if (hasColonCase && isColonList(node)) {
@@ -328,10 +367,15 @@ public class SynChk {
 				subNode = page.getNode(idx);
 				kwtyp = subNode.getKeywordTyp();
 				if (kwtyp != KeywordTyp.AS) {
+					oerr(currp, "Keyword 'import', expecting 'as', but was " +
+						"followed by invalid text: " + kwtyp.toString());
 					return 0;
 				}
+				ascurrp = currp;
 				currp = subNode.getRightp();
 				if (currp <= 0) {
+					oerr(ascurrp, "Keyword 'import', then 'as' found, " +
+						"not followed by module names");
 					return 0;
 				}
 				page = store.getPage(currp);
@@ -342,10 +386,14 @@ public class SynChk {
 				else if (hasColonCase && celltyp == NodeCellTyp.PTR && 
 					isColonList(subNode)) { } 
 				else {
+					oerr(ascurrp, "Keyword 'import', then 'as' found, " +
+						"followed by invalid text");
 					return 0;
 				}
 				currp = subNode.getRightp();
 				if (currp <= 0) {
+					oerr(ascurrp, "Keyword 'import', then 'as' clause found, " +
+						"followed by invalid text");
 					return 0;
 				}
 				page = store.getPage(currp);
@@ -353,17 +401,21 @@ public class SynChk {
 				subNode = page.getNode(idx);
 				celltyp = subNode.getDownCellTyp();
 				if (celltyp != NodeCellTyp.ID) {
+					oerr(ascurrp, "Keyword 'import', then 'as' clause found, " +
+						"not followed by identifier");
 					return 0;
 				}
 				currp = subNode.getRightp();
 				if (currp > 0) {
+					oerr(ascurrp, "Keyword 'import', then 'as' clause found, " +
+						"then identifier found, followed by invalid text");
 					return 0;
 				}
 			}
 			rightp = node.getRightp();
 		}
 		if (modCount <= 0) {
-			return 0;
+			return -1;
 		}
 		return savep;
 	}
@@ -374,6 +426,7 @@ public class SynChk {
 		KeywordTyp kwtyp;
 		NodeCellTyp celltyp;
 		int rightp;
+		int colonp;
 		int count = 0;
 		
 		rightp = node.getDownp();		
@@ -385,8 +438,11 @@ public class SynChk {
 		node = page.getNode(idx);
 		kwtyp = node.getKeywordTyp();
 		if (kwtyp != KeywordTyp.DOT) {
+			oerr(rightp, "Expecting colon operator, instead found: " +
+				kwtyp.toString());
 			return false;
 		}
+		colonp = rightp;
 		rightp = node.getRightp();
 		while (rightp > 0) {
 			count++;
@@ -395,11 +451,15 @@ public class SynChk {
 			node = page.getNode(idx);
 			celltyp = node.getDownCellTyp();
 			if (celltyp != NodeCellTyp.ID) {
+				oerr(rightp, "Expecting identifier after colon operator, " +
+					"invalid text found");
 				return false;
 			}
 			rightp = node.getRightp();
 		}
 		if (count < 2) {
+			oerr(colonp, "Expecting multiple identifiers after colon operator, " +
+				"less than 2 found");
 			return false;
 		}
 		return true;
@@ -573,8 +633,10 @@ public class SynChk {
 			return 1;
 		case VAR:
 			return 2;
-		case DECOR:
+		case GVAR:
 			return 3;
+		case DECOR:
+			return 4;
 		default:
 			return -1;
 		}
@@ -732,20 +794,37 @@ public class SynChk {
 	}
 	
 	private boolean chkDefParm(int rightp) {
+		return chkDefParmRtn(rightp, false);
+	}
+
+	private boolean isValidConstPair(int rightp) {
+		return chkDefParmRtn(rightp, true);
+	}
+
+	private boolean chkDefParmRtn(int rightp, boolean isConstPair) {
 		Page page;
 		int idx;
 		Node node;
-		NodeCellTyp celltyp;
+		NodeCellTyp celltyp, inCellTyp;
 		
 		page = store.getPage(rightp);
 		idx = store.getElemIdx(rightp);
 		node = page.getNode(idx);
 		celltyp = node.getDownCellTyp();
-		if (celltyp != NodeCellTyp.ID) {
+		if (isConstPair) {
+			inCellTyp = NodeCellTyp.FUNC;
+		}
+		else {
+			inCellTyp = NodeCellTyp.ID;
+		}
+		if (celltyp != inCellTyp) {
+			out("chkDefParm (): celltyp = " + celltyp);
+			out("chkDefParm (): fail 0");
 			return false;
 		}
 		rightp = node.getRightp();
 		if (!isConstExpr(rightp)) {
+			out("chkDefParm (): fail 1");
 			return false;
 		}
 		return true;
@@ -875,15 +954,6 @@ public class SynChk {
 	}
 	
 	private int chkDefunStmt(int rightp) {
-		return doDefunStmt(rightp, false);
-	}
-	
-	@SuppressWarnings("unused")
-	private int chkAbDefunStmt(int rightp) {
-		return doDefunStmt(rightp, true);
-	}
-	
-	private int doDefunStmt(int rightp, boolean isAbDef) {
 		Page page;
 		int idx;
 		Node node, parNode;
@@ -901,8 +971,81 @@ public class SynChk {
 			if (node != null) {
 				kwtyp = node.getKeywordTyp();
 			}
-			else if (isAbDef) {
+			else if (parNode.getKeywordTyp() == KeywordTyp.DO) {
+				break;
+			}
+			else {
+				out("chkDefunStmt (): fail 0");
 				return -1;
+			}
+			rightp = node.getRightp();
+			phaseNo = getDefunPhase(kwtyp);
+			if (phaseNo <= oldPhaseNo) {
+				out("chkDefunStmt (): fail 1");
+				return -1;
+			}
+			if (phaseNo > 1 && !isParms) {
+				out("chkDefunStmt (): fail 2");
+				return -1;
+			}
+			switch (phaseNo) {
+			case 1:
+				if (!chkParmList(rightp)) {
+					out("chkDefunStmt (): fail 3");
+					return -1;
+				}
+				isParms = true;
+				break;
+			case 2:
+			case 3:
+				if (chkVarList(rightp) < 0) {
+					out("chkDefunStmt (): fail 4");
+					return -1;
+				}
+				break;
+			case 4:
+				if (!chkDecorList(rightp)) {
+					out("chkDefunStmt (): fail 5");
+					return -1;
+				}
+				break;
+			default:
+				out("chkDefunStmt (): fail 6");
+				return -1;
+			}
+			oldPhaseNo = phaseNo;
+			rightp = parNode.getRightp();
+		}
+		if (!isParms) {
+			out("chkDefunStmt (): fail 7");
+			return -1;
+		}
+		out("gdefun: chk DO");
+		rightp = chkDoBlock(rightp);
+		if (rightp != 0) {
+			out("chkDefunStmt (): fail 1");
+			return -1;
+		}
+		return savep;
+	}
+	
+	private int chkAbDefunStmt(int rightp) {
+		Page page;
+		int idx;
+		Node node, parNode;
+		KeywordTyp kwtyp;
+		int savep = rightp;
+		int phaseNo = 0;
+		int oldPhaseNo = 0;
+		boolean isParms = false;
+
+		while (rightp > 0) {
+			page = store.getPage(rightp);
+			idx = store.getElemIdx(rightp);
+			parNode = page.getNode(idx);
+			node = store.getSubNode(parNode);
+			if (node != null) {
+				kwtyp = node.getKeywordTyp();
 			}
 			else if (parNode.getKeywordTyp() == KeywordTyp.DO) {
 				break;
@@ -926,14 +1069,9 @@ public class SynChk {
 				isParms = true;
 				break;
 			case 2:
-				if (isAbDef) {
-					return -1;
-				}
-				if (chkVarList(rightp) < 0) {
-					return -1;
-				}
-				break;
 			case 3:
+				return -1;
+			case 4:
 				if (!chkDecorList(rightp)) {
 					return -1;
 				}
@@ -947,11 +1085,8 @@ public class SynChk {
 		if (!isParms) {
 			return -1;
 		}
-		if (isAbDef) {
-			return savep;
-		}
-		out("gdefun: chk DO");
-		rightp = chkDoBlock(rightp);
+		out("abdefun: end loop");
+		//rightp = chkDoBlock(rightp);
 		if (rightp != 0) {
 			return -1;
 		}
@@ -967,7 +1102,18 @@ public class SynChk {
 		int oldPhaseNo = 0;
 		boolean isNameFound = false;
 		boolean isDotOK = true;
+		boolean isAbCls = (kwtyp == KeywordTyp.ABCLASS);
 
+		switch (kwtyp) {
+		case SCOOL:
+		case ISCOOL:
+			return chkScoolStmt(rightp);
+		case ENUM:
+		case IENUM:
+			return chkEnumStmt(rightp);
+		default:
+			break;
+		}
 		while (rightp > 0) {
 			page = store.getPage(rightp);
 			idx = store.getElemIdx(rightp);
@@ -1022,6 +1168,7 @@ public class SynChk {
 			}
 			if (!isNameFound && (phaseNo > 1)) {
 				out("chkClassStmt (): fail 4");
+				oerr(rightp, "Class definition: Name of class not found");
 				return -1;
 			}
 			if (!isDotOK) {
@@ -1032,7 +1179,7 @@ public class SynChk {
 			rightp = parNode.getRightp();
 		}
 		out("class: chk DO");
-		rightp = chkDoDefBlock(rightp);
+		rightp = chkDoDefBlock(rightp, isAbCls);
 		if (rightp != 0) {
 			out("chkClassStmt (): fail 6");
 			return -1;
@@ -1065,6 +1212,243 @@ public class SynChk {
 			out("getClassPhase (): fail 1");
 			return -1;
 		}
+	}
+	
+	private int chkScoolStmt(int rightp) {
+		Page page;
+		int idx;
+		Node node;
+		Node parNode;
+		int savep = rightp;
+		int phaseNo = 0;
+		int oldPhaseNo = 0;
+		boolean isNameFound = false;
+		KeywordTyp kwtyp = null;
+
+		while (rightp > 0) {
+			page = store.getPage(rightp);
+			idx = store.getElemIdx(rightp);
+			parNode = page.getNode(idx);
+			node = store.getSubNode(parNode);
+			if (node != null) {
+				kwtyp = node.getKeywordTyp();
+			}
+			else if (parNode.getKeywordTyp() == KeywordTyp.DO) {
+				break;
+			}
+			else if (parNode.getDownCellTyp() == NodeCellTyp.ID) {
+				kwtyp = KeywordTyp.NULL;
+			}
+			else {
+				out("chkScoolStmt (): fail 0");
+				return -1;
+			}
+			phaseNo = getScoolPhase(kwtyp, oldPhaseNo);
+			if (phaseNo <= oldPhaseNo) {
+				out("chkScoolStmt (): fail 1");
+				return -1;
+			}
+			switch (phaseNo) {
+			case 1:
+				isNameFound = true;
+				break;
+			case 2:
+				// does
+				rightp = node.getRightp();
+				if (!isValidDoesList(rightp)) {
+					out("chkScoolStmt (): fail 2");
+					return -1;
+				}
+				break;
+			case 3:
+				// const
+				rightp = node.getRightp();
+				if (!isValidConstList(rightp)) {
+					out("chkScoolStmt (): fail 3");
+					return -1;
+				}
+				break;
+			default:
+				break;
+			}
+			if (!isNameFound && (phaseNo > 1)) {
+				out("chkScoolStmt (): fail 4");
+				return -1;
+			}
+			oldPhaseNo = phaseNo;
+			rightp = parNode.getRightp();
+		}
+		out("scool: chk DO");
+		rightp = chkScoolDoBlock(rightp);
+		if (rightp != 0) {
+			out("chkScoolStmt (): fail 5");
+			return -1;
+		}
+		return savep;
+	}
+	
+	private int getScoolPhase(KeywordTyp kwtyp, int phaseNo) {
+		switch (kwtyp) {
+		case NULL:
+			return 1;
+		case DOES:
+			return 2;
+		case CONST:
+			return 3;
+		default:
+			out("getScoolPhase (): fail 1");
+			return -1;
+		}
+	}
+	
+	private boolean isValidConstList(int rightp) {
+		Page page;
+		int idx;
+		Node node;
+		int downp;
+		int count = 0;
+		
+		while (rightp > 0) {
+			count++;
+			page = store.getPage(rightp);
+			idx = store.getElemIdx(rightp);
+			node = page.getNode(idx);
+			downp = node.getDownp();
+			if (downp == 0 || !isValidConstPair(downp)) {
+				if (downp == 0) {
+					out("isValidConstList (): zero downp!");
+				}
+				out("isValidConstList (): count = " + count);
+				out("isValidConstList (): fail 1");
+				return false;
+			}
+			rightp = node.getRightp();
+		}
+		out("isValidConstList (): bottom");
+		return count > 0;
+	}
+			
+	private int chkEnumStmt(int rightp) {
+		Page page;
+		int idx;
+		Node node, subNode;
+		int savep = rightp;
+		int count = 0;
+		int enumTyp = -1;  // 0: id, 1: int, 2: char
+		int etyp;
+		
+		if (rightp == 0) {
+			return -1;
+		}
+		page = store.getPage(rightp);
+		idx = store.getElemIdx(rightp);
+		node = page.getNode(idx);
+		if (node.getDownCellTyp() != NodeCellTyp.ID) {
+			return -1;
+		} 
+		rightp = node.getRightp();
+		while (rightp > 0) {
+			count++;
+			page = store.getPage(rightp);
+			idx = store.getElemIdx(rightp);
+			node = page.getNode(idx);
+			subNode = store.getSubNode(node);
+			if (subNode != null) {
+				etyp = getEnumPair(subNode);
+			}
+			else {
+				etyp = getEnumTyp(node);
+			}
+			if (etyp < 0) {
+				return -1;
+			}
+			if (enumTyp < 0) {
+				enumTyp = etyp;
+			}
+			else if (etyp != enumTyp) {
+				return -1;
+			}
+			rightp = node.getRightp();
+		}
+		if (count == 0) {
+			return -1;
+		}
+		return savep;
+	}
+	
+	private int getEnumTyp(Node node) {
+		int len;
+		
+		switch (node.getDownCellTyp()) {
+		case ID: return 0;
+		case INT: return 1;
+		case STRING:
+			len = getStrNodeLen(node);
+			if (len != 1) {
+				return -1;
+			}
+			return 2;
+		default:
+			return -1;
+		}
+	}
+	
+	private int getEnumPair(Node node) {
+		Page page;
+		int idx;
+		int enumTyp, etyp;
+		int rightp;
+		
+		if (node.getKeywordTyp() != KeywordTyp.DOT) {
+			return -1;
+		}
+		rightp = node.getRightp();
+		if (rightp <= 0) {
+			out("getEnumPair (): fail 0");
+			return -1;
+		}
+		page = store.getPage(rightp);
+		idx = store.getElemIdx(rightp);
+		node = page.getNode(idx);
+		enumTyp = getEnumTyp(node);
+		rightp = node.getRightp();
+		if (rightp <= 0) {
+			out("getEnumPair (): fail 0.5");
+			return -1;
+		}
+		page = store.getPage(rightp);
+		idx = store.getElemIdx(rightp);
+		node = page.getNode(idx);
+		etyp = getEnumTyp(node);
+		rightp = node.getRightp();
+		if (rightp > 0) {
+			out("getEnumPair (): fail 1");
+			return -1;
+		}
+		if (etyp != enumTyp) {
+			out("getEnumPair (): fail 2");
+			return -1;
+		}
+		out("getEnumPair() = " + enumTyp);
+		return enumTyp;
+	}
+	
+	private int getStrNodeLen(Node node) {
+		Page page;
+		int idx;
+		int downp;
+		String str;
+		int len;
+
+		if (node.getDownCellTyp() != NodeCellTyp.STRING) {
+			return -1;
+		}
+		downp = node.getDownp();
+		page = store.getPage(downp);
+		idx = store.getElemIdx(downp);
+		str = page.getString(idx);
+		len = str.length();
+		return len;
 	}
 	
 	private boolean isValidDotList(int rightp) {
@@ -1112,13 +1496,14 @@ public class SynChk {
 		return isValid;
 	}
 	
-	private int chkDoDefBlock(int rightp) {
+	private int chkDoDefBlock(int rightp, boolean isAbCls) {
 		Page page;
 		int idx;
 		Node node, subNode;
 		KeywordTyp kwtyp;
 		int downp, downq;
 		int phaseNo;
+		boolean abfound;
 		
 		if (rightp <= 0) {
 			out("doDefBlock (): fail 0");
@@ -1146,33 +1531,115 @@ public class SynChk {
 			page = store.getPage(rightp);
 			idx = store.getElemIdx(rightp);
 			node = page.getNode(idx);
-			if (node.isOpenPar()) {
-				downp = node.getDownp();
-				page = store.getPage(downp);
-				idx = store.getElemIdx(downp);
-				subNode = page.getNode(idx);
-				kwtyp = subNode.getKeywordTyp();
-				if (kwtyp == KeywordTyp.ZPAREN) {
-					break;
-				}
-				phaseNo = getPhaseNo(kwtyp);
-				if (phaseNo != 3) {
-					//out("phaseNo = " + phaseNo + ", kwtyp = " + kwtyp);
-					out("doDefBlock (): fail 3");
-					return -1;
-				}
-				else {
-					out("doDefBlock (): OK - 3");
-				}
-				downp = subNode.getRightp();
-				downq = chkDefunStmt(downp);
-				if (downq <= 0) {
-					out("doDefBlock (): fail 4");
-					return -1;
-				}
+			if (!node.isOpenPar()) {
+				out("doDefBlock (): fail 5");
+				return -1;
+			}
+			downp = node.getDownp();
+			page = store.getPage(downp);
+			idx = store.getElemIdx(downp);
+			subNode = page.getNode(idx);
+			kwtyp = subNode.getKeywordTyp();
+			if (kwtyp == KeywordTyp.ZPAREN) {
+				break;
+			}
+			phaseNo = getPhaseNo(kwtyp);
+			switch (phaseNo) {
+			case 3:
+				abfound = false;
+				break;
+			case ABPHASE:
+				abfound = true;
+				break;
+			default:
+				out("doDefBlock (): fail 3");
+				return -1;
+			}
+			out("doDefBlock (): OK - 3");
+			downp = subNode.getRightp();
+			if (abfound && isAbCls) {
+				downq = chkAbDefunStmt(downp);
+			}
+			else if (abfound) {
+				out("doDefBlock (): fail 4");
+				return -1;
 			}
 			else {
+				downq = chkDefunStmt(downp);
+			}
+			if (downq <= 0) {
 				out("doDefBlock (): fail 5");
+				return -1;
+			}
+			rightp = node.getRightp();
+		}
+		return 0; // OK
+	}
+	
+	private int chkScoolDoBlock(int rightp) {
+		Page page;
+		int idx;
+		Node node, subNode;
+		KeywordTyp kwtyp;
+		int downp, downq;
+		
+		if (rightp <= 0) {
+			out("doScoolBlock (): fail 0");
+			return -1;
+		}
+		page = store.getPage(rightp);
+		idx = store.getElemIdx(rightp);
+		node = page.getNode(idx);
+		kwtyp = node.getKeywordTyp();
+		if (kwtyp != KeywordTyp.DO) {
+			out("doScoolBlock (): fail 1");
+			return -1;
+		}
+		if (!node.isOpenPar()) {
+			out("doScoolBlock (): fail 1.5");
+			return -1;
+		}
+		rightp = node.getRightp();
+		if (rightp > 0) {
+			out("doScoolBlock (): fail 2");
+			return -1;
+		}
+		rightp = node.getDownp();
+		while (rightp > 0) {
+			page = store.getPage(rightp);
+			idx = store.getElemIdx(rightp);
+			node = page.getNode(idx);
+			if (!node.isOpenPar()) {
+				out("doScoolBlock (): fail 3");
+				return -1;
+			}
+			downp = node.getDownp();
+			page = store.getPage(downp);
+			idx = store.getElemIdx(downp);
+			subNode = page.getNode(idx);
+			kwtyp = subNode.getKeywordTyp();
+			if (kwtyp == KeywordTyp.ZPAREN) {
+				break;
+			}
+			downp = subNode.getRightp();
+			if (downp == 0) {
+				out("doScoolBlock (): fail 4");
+				return -1;
+			}
+			switch (kwtyp) {
+			case ABDEFUN:
+				downq = chkAbDefunStmt(downp);
+				break;
+			case DEFIMP:
+				downq = chkDefunStmt(downp);
+				break;
+			default:
+				out("doScoolBlock (): fail 5");
+				return -1;
+			}
+			out("doScoolBlock (): OK - 5");
+			if (downq <= 0) {
+				out("doScoolBlock (): fail 6");
 				return -1;
 			}
 			rightp = node.getRightp();
