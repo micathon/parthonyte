26e4397bc3adc8be48eccf22b82e488d1a0dc966
Mike Hahn <hahnbytes@gmail.com>
2020-05-19 20:55:20 -0400

    Add files via upload

--- src/coop.java
+++ src/coop.java
@@ -5,15 +5,26 @@ public class coop {
 	public static void main(String[] args) {
 		InitMain initobj = new InitMain();
 		String fileName = "";
+		Boolean isUnitTest = false;
 
-		if (args.length > 1) {
+		if (args.length > 2) {
 			System.out.println("Usage: java coop [filename]");
 			return;
 		}
 		if (args.length == 1) {
 			fileName = "../dat/" + args[0] + ".coop";
 		}
-		initobj.runInit(fileName);
+		else if (args.length == 2) {
+			if (args[1].equals("-u")) {
+				fileName = "../dat/test/" + args[0] + ".test";
+				isUnitTest = true;
+			}
+			else {
+				System.out.println("Usage: java coop filename [-u]");
+				return;
+			}
+		}
+		initobj.runInit(fileName, isUnitTest);
 	}
 
 }
