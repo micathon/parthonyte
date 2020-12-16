bcae5eeb57139d08080c3622a044844bcadb969e
Mike Hahn <hahnbytes@gmail.com>
2020-12-15 20:47:36 -0500

    Add files via upload

--- src/runtm/RunTime.java
+++ src/runtm/RunTime.java
@@ -10,23 +10,31 @@ import page.Node;
 import page.AddrNode;
 import page.Store;
 import page.Page;
+import scansrc.ScanSrc;
+import synchk.SynChk;
 
 // Code Execution
 
 public class RunTime implements IConst {
 
 	private Store store;
+	private ScanSrc scanSrc;
+	private SynChk synChk;
 	private static final boolean isSilent = false;
 	private int rootNodep;
 
-	public RunTime(Store store, int rootNodep) {
+	public RunTime(Store store, ScanSrc scanSrc, SynChk synChk,	int rootNodep) {
 		this.store = store;
+		this.scanSrc = scanSrc;
+		this.synChk = synChk;
 		this.rootNodep = rootNodep;
 	}
 
 	public boolean run() {
+		boolean rtnval;
 		omsg("RunTime.run: rootNodep = " + rootNodep);
-		return true;
+		rtnval = runRoot(rootNodep);
+		return rtnval;
 	}
 	
 	public void out(String msg) {
@@ -39,6 +47,132 @@ public class RunTime implements IConst {
 		if (!isSilent) {
 			System.out.println(msg);
 		}
+	}
+	
+	private boolean runRoot(int rightp) {
+		int downp;
+		Node node;
+		boolean rtnval;
+		
+		node = store.getNode(rightp);
+		rightp = node.getRightp();
+		node = store.getNode(rightp);
+		downp = node.getDownp();
+		rtnval = runTopBlock(downp);
+		return rtnval;
+	}
+	
+	private boolean runTopBlock(int rightp) {
+		// process top-level stmts.
+		Node node;
+		int downp;
+		KeywordTyp kwtyp;
+		NodeCellTyp celltyp;
+		int phaseNo = 0;
+
+		while (rightp != 0) {
+			node = store.getNode(rightp);
+			kwtyp = node.getKeywordTyp();
+			celltyp = node.getDownCellTyp();
+			out("rightp = " + rightp +  
+				", kwd = " + kwtyp + ", celtyp = " + celltyp);
+			if (kwtyp != KeywordTyp.ZSTMT) {
+				return false;
+			}
+			if (node.isOpenPar()) {
+				out("Here is (");
+				downp = node.getDownp();
+				phaseNo = runTopStmt(downp, phaseNo);
+				if (phaseNo < 0) {
+					return false;
+				}
+				out("Here is )");
+			}
+			else {
+				return false;
+			}
+			rightp = node.getRightp();
+			if (phaseNo < 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private int runTopStmt(int rightp, int phaseNo) {
+		// process top-level statement
+		// return phase no. of current stmt.: 0=quest, 1=import,
+		//   gdefun, functions, 4=classes
+		// return -1 on error
+		Node node;
+		KeywordTyp kwtyp = null;
+		NodeCellTyp celltyp;
+		boolean first = true;
+		int currPhaseNo = phaseNo;
+		int rightq;
+
+		while (rightp > 0) {
+			node = store.getNode(rightp);
+			kwtyp = node.getKeywordTyp();
+			celltyp = node.getDownCellTyp();
+			out("rightp = " + rightp +  
+					", kwd = " + kwtyp + ", celtyp = " + celltyp);
+			if (first) {
+				// at keyword token, beginning of top-level stmt.
+				out("Statement kwd = " + kwtyp);
+				currPhaseNo = synChk.getPhaseNo(kwtyp);
+				rightq = rightp;
+				rightp = node.getRightp();
+				if (rightp <= 0) {
+					return -1;
+				}
+				// rightp > 0 inside following switch
+				switch (currPhaseNo) {
+				case 0:
+					return -1;
+				case 1:
+					rightq = runImportStmt(rightp, kwtyp);
+					break;
+				case 2:
+					rightq = runGlbDefStmt(rightp);
+					break;
+				case 3:
+					rightq = runDefunStmt(rightp);
+					break;
+				case 4:
+					rightq = runClassStmt(rightp, kwtyp);
+					break;
+				default:
+					rightq = -1;
+				}
+				if (rightq > 0) {
+					rightp = rightq;
+				}
+				else {
+					return -1;
+				}
+			}
+			rightp = node.getRightp();
+			first = false;
+		}
+		return currPhaseNo;
+	}
+	
+	private int runImportStmt(int rightp, KeywordTyp kwtyp) {
+		return rightp;
+	}
+	
+	private int runGlbDefStmt(int rightp) {
+		omsg("Keyword gdefun detected.");
+		return rightp;
+	}
+	
+	private int runDefunStmt(int rightp) {
+		return rightp;
+	}
+	
+	private int runClassStmt(int rightp, KeywordTyp kwtyp) {
+		return rightp;
 	}
 	
 }
