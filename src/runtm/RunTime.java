81e2bdeeb2492e80b7a7574b2cd5bc14252770cb
Mike Hahn <hahnbytes@gmail.com>
2021-02-13 20:29:02 -0500

    Add files via upload

--- src/runtm/RunTime.java
+++ src/runtm/RunTime.java
@@ -24,9 +24,12 @@ public class RunTime implements IConst {
 	private SynChk synChk;
 	private static final boolean isSilent = false;
 	private int rootNodep;
+	private HashMap<String, Integer> glbFunMap;
 	private HashMap<String, Integer> glbLocVarMap;
 	private ArrayList<Integer> glbLocVarList;
 	private ArrayList<AddrNode> locVarList;
+	private String scopeFuncName;
+	private int defunCount;
 	private int count;
 
 	public RunTime(Store store, ScanSrc scanSrc, SynChk synChk,	int rootNodep) {
@@ -34,9 +37,11 @@ public class RunTime implements IConst {
 		this.scanSrc = scanSrc;
 		this.synChk = synChk;
 		this.rootNodep = rootNodep;
+		glbFunMap = new HashMap<String, Integer>();
 		glbLocVarMap = new HashMap<String, Integer>();
 		glbLocVarList = new ArrayList<Integer>();
 		locVarList = new ArrayList<AddrNode>();
+		defunCount = 0;
 		count = 0;
 	}
 
@@ -72,9 +77,14 @@ public class RunTime implements IConst {
 		node = store.getNode(rightp);
 		downp = node.getDownp();
 		rtnval = scanTopBlock(downp);
-		if (rtnval) {
-			rtnval = runTopBlock(downp);
+		if (!rtnval) {
+			return false;
 		}
+		rtnval = scopeTopBlock(downp);
+		if (!rtnval) {
+			return false;
+		}
+		rtnval = runTopBlock(downp);
 		return rtnval;
 	}
 	
@@ -99,6 +109,44 @@ public class RunTime implements IConst {
 				out("Here is (");
 				downp = node.getDownp();
 				phaseNo = scanTopStmt(downp, phaseNo);
+				if (phaseNo < 0) {
+					return false;
+				}
+				out("Here is )");
+			}
+			else {
+				return false;
+			}
+			rightp = node.getRightp();
+			if (phaseNo < 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	private boolean scopeTopBlock(int rightp) {
+		// process top-level stmts.
+		// scope = scan + code
+		Node node;
+		int downp;
+		KeywordTyp kwtyp;
+		NodeCellTyp celltyp;
+		int phaseNo = 0;
+
+		while (rightp != 0) {
+			node = store.getNode(rightp);
+			kwtyp = node.getKeywordTyp();
+			celltyp = node.getDownCellTyp();
+			out("rightp = " + rightp +  
+				", kwd = " + kwtyp + ", celtyp = " + celltyp);
+			if (kwtyp != KeywordTyp.ZSTMT) {
+				return false;
+			}
+			if (node.isOpenPar()) {
+				out("Here is (");
+				downp = node.getDownp();
+				phaseNo = scopeTopStmt(downp, phaseNo);
 				if (phaseNo < 0) {
 					return false;
 				}
@@ -211,6 +259,65 @@ public class RunTime implements IConst {
 		return currPhaseNo;
 	}
 	
+	private int scopeTopStmt(int rightp, int phaseNo) {
+		// process top-level statement
+		// return phase no. of current stmt.: 0=quest, 1=import,
+		//   gdefun, functions, 4=classes
+		// return -1 on error
+		Node node;
+		KeywordTyp kwtyp = null;
+		NodeCellTyp celltyp;
+		boolean first = true;
+		int currPhaseNo = phaseNo;
+		int rightq;
+
+		while (rightp > 0) {
+			node = store.getNode(rightp);
+			kwtyp = node.getKeywordTyp();
+			celltyp = node.getDownCellTyp();
+			out("rightp = " + rightp +  
+					", kwd = " + kwtyp + ", celtyp = " + celltyp);
+			if (first) {
+				// at keyword token, beginning of top-level stmt.
+				out("Statement kwd = " + kwtyp);
+				currPhaseNo = synChk.getPhaseNo(kwtyp);
+				rightq = rightp;
+				rightp = node.getRightp();
+				if (rightp <= 0) {
+					return -1;
+				}
+				// rightp > 0 inside following switch
+				switch (currPhaseNo) {
+				case 0:
+					return -1;
+				case 1:
+					rightq = scopeImportStmt(rightp, kwtyp);
+					break;
+				case 2:
+					rightq = scopeGlbDefStmt(rightp);
+					break;
+				case 3:
+					rightq = scopeDefunStmt(rightp);
+					break;
+				case 4:
+					rightq = scopeClassStmt(rightp, kwtyp);
+					break;
+				default:
+					rightq = -1;
+				}
+				if (rightq > 0) {
+					rightp = rightq;
+				}
+				else {
+					return -1;
+				}
+			}
+			rightp = node.getRightp();
+			first = false;
+		}
+		return currPhaseNo;
+	}
+	
 	private int runTopStmt(int rightp, int phaseNo) {
 		// process top-level statement
 		// return phase no. of current stmt.: 0=quest, 1=import,
@@ -270,6 +377,10 @@ public class RunTime implements IConst {
 		return rightp;
 	}
 	
+	private int scopeImportStmt(int rightp, KeywordTyp kwtyp) {
+		return rightp;
+	}
+	
 	private int scanGlbDefStmt(int rightp) {
 		Node node;
 		Node firstNode;
@@ -279,12 +390,11 @@ public class RunTime implements IConst {
 		int savep = rightp;
 		String varName;
 		int varidx = 0;
-		int stmtCount = 0;
 		int idx;
 		Page page;
-		boolean rtnval;
 
 		omsg("Keyword gdefun detected.");
+		glbFunMap.put(getGdefunWord(), defunCount++);
 		node = store.getNode(rightp);
 		firstNode = node;
 		kwtyp = node.getKeywordTyp();
@@ -305,7 +415,7 @@ public class RunTime implements IConst {
 					return -1;
 				}
 				downp = node.getDownp();
-				varName = getGdefunWord();
+				varName = getGdefunWord() + ' ';
 				varName += store.getVarName(downp);
 				glbLocVarMap.put(varName, varidx++);
 				glbLocVarList.add(rightp);
@@ -332,6 +442,46 @@ public class RunTime implements IConst {
 			omsg("Missing DO");
 			return -1;
 		}
+		return savep;
+	}
+	
+	private int scopeGlbDefStmt(int rightp) {
+		Node node;
+		Node firstNode;
+		KeywordTyp kwtyp;
+		int downp;
+		int savep = rightp;
+		int stmtCount = 0;
+		boolean rtnval;
+
+		omsg("Keyword gdefun detected.");
+		scopeFuncName = getGdefunWord();
+		node = store.getNode(rightp);
+		firstNode = node;
+		kwtyp = node.getKeywordTyp();
+		if (kwtyp == KeywordTyp.ZPAREN) {
+			rightp = node.getDownp();
+			node = store.getNode(rightp);
+			kwtyp = node.getKeywordTyp();
+			if (kwtyp != KeywordTyp.VAR) {
+				return -1;
+			}
+			rightp = firstNode.getRightp();
+			node = store.getNode(rightp);
+			kwtyp = node.getKeywordTyp();
+			if (kwtyp == KeywordTyp.ZPAREN) {  // (ivar ...)
+				rightp = node.getRightp();
+				if (rightp <= 0) {
+					return -1;
+				}
+				node = store.getNode(rightp);
+			}
+			kwtyp = node.getKeywordTyp();
+		}
+		if (kwtyp != KeywordTyp.DO) {
+			omsg("Missing DO");
+			return -1;
+		}
 		rightp = node.getDownp();
 		while (rightp > 0) {
 			node = store.getNode(rightp);
@@ -339,6 +489,7 @@ public class RunTime implements IConst {
 			if (kwtyp != KeywordTyp.ZSTMT) {
 				return -1;
 			}
+			omsg("Stmt count = " + stmtCount);
 			downp = node.getDownp();
 			rtnval = scopeStmt(downp);
 			if (!rtnval) {
@@ -495,7 +646,7 @@ public class RunTime implements IConst {
 			return false;
 		}
 		downp = node.getDownp();
-		varName = getGdefunWord();
+		varName = scopeFuncName + ' ';
 		varName += store.getVarName(downp);
 		value = glbLocVarMap.get(varName);
 		if (value == null) {
@@ -512,10 +663,166 @@ public class RunTime implements IConst {
 	}
 	
 	private int scanDefunStmt(int rightp) {
-		return rightp;
+		Node node;
+		Node upNode;
+		KeywordTyp kwtyp;
+		NodeCellTyp celltyp;
+		int downp;
+		int savep = rightp;
+		String funcName;
+		String varName;
+		int varidx = 0;
+		int idx;
+		Page page;
+
+		omsg("Keyword defun detected.");
+		node = store.getNode(rightp);
+		upNode = node;
+		kwtyp = node.getKeywordTyp();
+		if (kwtyp != KeywordTyp.ZPAREN) {
+			return -1;
+		}
+		rightp = node.getDownp();
+		node = store.getNode(rightp);
+		celltyp = node.getDownCellTyp();
+		if (celltyp != NodeCellTyp.FUNC) {
+			return -1;
+		}
+		downp = node.getDownp();
+		funcName = store.getVarName(downp);
+		glbFunMap.put(funcName, defunCount++);
+		node = upNode;
+		rightp = node.getRightp();
+		node = store.getNode(rightp);
+		kwtyp = node.getKeywordTyp();
+		if (kwtyp == KeywordTyp.ZPAREN) {
+			upNode = node;
+			rightp = node.getDownp();
+			node = store.getNode(rightp);
+			kwtyp = node.getKeywordTyp();
+			if (kwtyp != KeywordTyp.VAR) {
+				return -1;
+			}
+			rightp = node.getRightp();
+			while (rightp > 0) {
+				page = store.getPage(rightp);
+				idx = store.getElemIdx(rightp);
+				node = page.getNode(idx);
+				celltyp = node.getDownCellTyp();
+				if (celltyp != NodeCellTyp.ID) {
+					return -1;
+				}
+				downp = node.getDownp();
+				varName = funcName + ' ';
+				varName += store.getVarName(downp);
+				glbLocVarMap.put(varName, varidx++);
+				glbLocVarList.add(rightp);
+				rightp = node.getRightp();
+				node.setRightp(downp);
+				node.setDownp(0);
+				node.setRightCell(true);
+				page.setNode(idx, node);
+			}
+			node = upNode;
+			rightp = node.getRightp();
+			omsg("Global/local var count = " + varidx);
+			node = store.getNode(rightp);
+			kwtyp = node.getKeywordTyp();
+			if (kwtyp == KeywordTyp.ZPAREN) {  // (gvar ...)
+				rightp = node.getRightp();
+				if (rightp <= 0) {
+					return -1;
+				}
+				node = store.getNode(rightp);
+			}
+			kwtyp = node.getKeywordTyp();
+		}
+		if (kwtyp != KeywordTyp.DO) {
+			omsg("Missing DO");
+			return -1;
+		}
+		return savep;
+	}
+	
+	private int scopeDefunStmt(int rightp) {
+		Node node;
+		Node upNode;
+		KeywordTyp kwtyp;
+		NodeCellTyp celltyp;
+		int downp;
+		int savep = rightp;
+		int stmtCount = 0;
+		boolean rtnval;
+
+		omsg("Keyword (scope) defun detected.");
+		node = store.getNode(rightp);
+		upNode = node;
+		kwtyp = node.getKeywordTyp();
+		if (kwtyp != KeywordTyp.ZPAREN) {
+			return -1;
+		}
+		rightp = node.getDownp();
+		node = store.getNode(rightp);
+		celltyp = node.getDownCellTyp();
+		if (celltyp != NodeCellTyp.FUNC) {
+			return -1;
+		}
+		downp = node.getDownp();
+		scopeFuncName = store.getVarName(downp);
+		node = upNode;
+		rightp = node.getRightp();
+		node = store.getNode(rightp);
+		kwtyp = node.getKeywordTyp();
+		if (kwtyp == KeywordTyp.ZPAREN) {
+			upNode = node;
+			rightp = node.getDownp();
+			node = store.getNode(rightp);
+			kwtyp = node.getKeywordTyp();
+			if (kwtyp != KeywordTyp.VAR) {
+				return -1;
+			}
+			node = upNode;
+			rightp = node.getRightp();
+			node = store.getNode(rightp);
+			kwtyp = node.getKeywordTyp();
+			if (kwtyp == KeywordTyp.ZPAREN) {  // (gvar ...)
+				rightp = node.getRightp();
+				if (rightp <= 0) {
+					return -1;
+				}
+				node = store.getNode(rightp);
+			}
+			kwtyp = node.getKeywordTyp();
+		}
+		if (kwtyp != KeywordTyp.DO) {
+			omsg("Missing DO");
+			return -1;
+		}
+		rightp = node.getDownp();
+		while (rightp > 0) {
+			node = store.getNode(rightp);
+			kwtyp = node.getKeywordTyp();
+			if (kwtyp != KeywordTyp.ZSTMT) {
+				return -1;
+			}
+			omsg("Stmt count = " + stmtCount);
+			downp = node.getDownp();
+			rtnval = scopeStmt(downp);
+			if (!rtnval) {
+				return -1;
+			}
+			stmtCount++;
+			rightp = node.getRightp();
+		} 
+		omsg("Stmt count = " + stmtCount + ", set count = " + count);
+		return savep;
 	}
 	
 	private int scanClassStmt(int rightp, KeywordTyp kwtyp) {
+		return rightp;
+	}
+	
+	private int scopeClassStmt(int rightp, KeywordTyp kwtyp) {
 		return rightp;
 	}
 	
@@ -601,7 +908,7 @@ public class RunTime implements IConst {
 	}
 	
 	private String getGdefunWord() {
-		return "gdefun@";
+		return "gdefun";
 	}
 	
 }
