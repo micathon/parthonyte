565cd16f352fb6e06b91ba778407b87e817466d8
Mike Hahn <hahnbytes@gmail.com>
2021-02-14 21:51:08 -0500

    Add files via upload

--- src/page/AddrNode.java
+++ src/page/AddrNode.java
@@ -37,5 +37,49 @@ public class AddrNode {
 		short mask = (short)(pgtyp.ordinal());
 		header = (short)((header & 0xFFF0) | mask);
 	}
+	
+	public int getHdrLocVarTyp() {
+		short locVarTyp = (short)((header & (short)0x0030) >>> 4);
+		return (int)locVarTyp;
+	}
 
+	public void setHdrLocVarTyp(int locVarTyp) {
+		int mask = 0xFFCF;
+		header &= mask;
+		locVarTyp &= 0x3;
+		header |= (locVarTyp << 4);
+	}
+	
+	public void setHdrNonVar() {
+		setHdrLocVarTyp(0);
+	}
+	
+	public void setHdrLocVar() {
+		setHdrLocVarTyp(1);
+	}
+	
+	public void setHdrFldVar() {
+		setHdrLocVarTyp(2);
+	}
+	
+	public void setHdrGlbVar() {
+		setHdrLocVarTyp(3);
+	}
+	
+	public boolean getHdrNonVar() {
+		return (getHdrLocVarTyp() == 0);
+	}
+	
+	public boolean getHdrLocVar() {
+		return (getHdrLocVarTyp() == 1);
+	}
+	
+	public boolean getHdrFldVar() {
+		return (getHdrLocVarTyp() == 2);
+	}
+	
+	public boolean getHdrGlbVar() {
+		return (getHdrLocVarTyp() == 3);
+	}
+	
 }
