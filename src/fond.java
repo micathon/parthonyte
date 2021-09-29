ca63d468900b5b93f636f3b0a4720e586f22f7b3
Mike Hahn <hahnbytes@gmail.com>
2021-09-29 13:42:04 -0700

    Add files via upload

---
+++ src/fond.java
@@ -0,0 +1,32 @@
+import init.InitMain;
+
+public class fond {
+
+	public static void main(String[] args) {
+		InitMain initobj = new InitMain();
+		String fileName = "";
+		boolean isUnitTest = false;
+		boolean isMain = false;
+
+		if (args.length > 2) {
+			System.out.println("Usage: java fond [filename]");
+			return;
+		}
+		if (args.length == 1) {
+			fileName = "../dat/" + args[0] + ".fond";
+		}
+		else if (args.length == 2) {
+			isMain = args[0].equals("main");
+			if (args[1].equals("-u")) {
+				fileName = "../dat/test/" + args[0] + ".test";
+				isUnitTest = true;
+			}
+			else {
+				System.out.println("Usage: java fond filename [-u]");
+				return;
+			}
+		}
+		initobj.runInit(fileName, isUnitTest, isMain);
+	}
+
+}
