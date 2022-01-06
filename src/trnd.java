353f894f11e9699a4a0be4fc38fa10829db37d16
Mike Hahn <hahnbytes@gmail.com>
2022-01-06 06:00:59 -0500

    Add files via upload

---
+++ src/trnd.java
@@ -0,0 +1,32 @@
+import init.InitMain;
+
+public class trnd {
+
+	public static void main(String[] args) {
+		InitMain initobj = new InitMain();
+		String fileName = "";
+		boolean isUnitTest = false;
+		boolean isMain = false;
+
+		if (args.length > 2) {
+			System.out.println("Usage: java trnd [filename]");
+			return;
+		}
+		if (args.length == 1) {
+			fileName = "../dat/" + args[0] + ".trnd";
+		}
+		else if (args.length == 2) {
+			isMain = args[0].equals("main");
+			if (args[1].equals("-u")) {
+				fileName = "../dat/test/" + args[0] + ".test";
+				isUnitTest = true;
+			}
+			else {
+				System.out.println("Usage: java trnd filename [-u]");
+				return;
+			}
+		}
+		initobj.runInit(fileName, isUnitTest, isMain);
+	}
+
+}
