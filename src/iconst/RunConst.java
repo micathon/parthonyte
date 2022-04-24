e5bf9d393521733dba9ee9d9752962fa82267892
Mike Hahn <hahnbytes@gmail.com>
2022-04-24 18:34:41 -0400

    Add files via upload

---
+++ src/iconst/RunConst.java
@@ -0,0 +1,31 @@
+package iconst;
+
+public interface RunConst {
+
+	int EXIT = -1;
+	int NEGADDR = -2;
+	int STKOVERFLOW = -3;
+	int STKUNDERFLOW = -4;
+	int BADSTMT = -5;
+	int BADOP = -6;
+	int BADCELLTYP = -7;
+	int BADALLOC = -8;
+	int BADPOP = -9;
+	int ZERODIV = -10;
+	int KWDPOPPED = -11;
+	int NULLPOPPED = -12;
+	int BADSTORE = -13;
+	int BADINTVAL = -14;
+	int STMTINEXPR = -15;
+	int BADZSTMT = -16;
+	int BADSETSTMT = -17;
+	int BADPARMCT = -18;
+	int RTNISEMPTY = -19;
+	int BADTYPE = -20;
+	int GENERR = -99;
+	int NEGBASEVAL = -1000;
+	int NONVAR = 0; // same as AddrNode
+	int LOCVAR = 1; //
+	int FLDVAR = 2; //
+	int GLBVAR = 3; //
+}
