4e5f47d933a122d61c0bcb8a9ef84ca5cf55836c
Mike Hahn <hahnbytes@gmail.com>
2023-05-15 12:17:58 -0400

    Add files via upload

---
+++ src/xyn.java
@@ -0,0 +1,37 @@
+import init.InitMain;
+
+public class xyn {
+
+	public static void main(String[] args) {
+		InitMain initobj = new InitMain();
+		String fileName = "";
+		boolean isUnitTest = false;
+		boolean isMain = false;
+		boolean isRunTest = false;
+
+		if (args.length > 2) {
+			System.out.println("Usage: java xyn [filename]");
+			return;
+		}
+		if (args.length == 1) {
+			fileName = "../dat/" + args[0] + ".xyn";
+		}
+		else if (args.length == 2) {
+			isMain = args[0].equals("main");
+			if (args[1].equals("-u")) {
+				fileName = "../dat/test/" + args[0] + ".test";
+				isUnitTest = true;
+			}
+			else if (args[1].equals("-r")) {
+				fileName = "../dat/rt/" + args[0] + ".test";
+				isRunTest = true;
+			}
+			else {
+				System.out.println("Usage: java xyn filename [-u][-r]");
+				return;
+			}
+		}
+		initobj.runInit(fileName, isUnitTest, isMain, isRunTest);
+	}
+
+}
