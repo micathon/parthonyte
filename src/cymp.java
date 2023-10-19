317cfc20c6ad6dd51d82ed7973bb429dfd67c9fc
Mike Hahn <hahnbytes@gmail.com>
2023-10-19 05:36:58 -0400

    Add files via upload

---
+++ src/cymp.java
@@ -0,0 +1,41 @@
+import init.InitMain;
+
+public class cymp {
+
+	public static void main(String[] args) {
+		InitMain initobj = new InitMain();
+		String fileName = "";
+		String filePath = "";
+		boolean isUnitTest = false;
+		boolean isMain = false;
+		boolean isRunTest = false;
+
+		if (args.length == 1) {
+			filePath = "../dat/";
+			fileName = args[0] + ".cymp";
+		}
+		else if (args.length == 2) {
+			isMain = args[0].equals("main");
+			if (args[1].equals("-u")) {
+				filePath = "../dat/test/";
+				fileName = args[0] + ".test";
+				isUnitTest = true;
+			}
+			else if (args[1].equals("-r")) {
+				filePath = "../dat/rt/";
+				fileName = args[0] + ".test";
+				isRunTest = true;
+			}
+			else {
+				System.out.println("Usage: java cymp filename [-u][-r]");
+				return;
+			}
+		}
+		else if (args.length > 2) {
+			System.out.println("Usage: java cymp [filename]");
+			return;
+		}
+		initobj.runInit(filePath, fileName, isUnitTest, isMain, isRunTest);
+	}
+
+}
